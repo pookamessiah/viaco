d0c19193746a6e4e4fc31e8dd9df06f94521d21e
pookamessiah <preciousogbuefi100@gmail.com>
2025-05-29 22:51:51 +0100

    Update app.py

--- Viaco-Delivery/app.py
+++ Viaco-Delivery/app.py
@@ -119,4 +119,6 @@ def track():
     return render_template("track.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
